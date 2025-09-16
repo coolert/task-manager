fb768ac92b08deb82382fb51d0c94839287b52e3
keith <1033610335@qq.com>
2025-09-16 21:24:04 +0800

    feat: add creator_id to tasks table

--- database/migrations/2025_09_14_123444_create_tasks_table.php
+++ database/migrations/2025_09_14_123444_create_tasks_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('project_id')->constrained();
+            $table->foreignId('creator_id')->constrained('users');
             $table->string('title', 150);
             $table->mediumText('description')->nullable();
             $table->enum('status', ['todo', 'doing', 'done'])->default('todo');
