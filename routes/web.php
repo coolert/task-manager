04c2822047d1c7c0d40a73c601d6b32856c6b511
keith <1033610335@qq.com>
2025-12-03 15:05:03 +0800

    chore: remove unused Laravel default resources and cleanup project structure

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 if (app()->environment(['local', 'development', 'testing'])) {
     Route::get('/openapi.yaml', function () {
         return response()->file(base_path('docs/openapi/openapi.yaml'), [
