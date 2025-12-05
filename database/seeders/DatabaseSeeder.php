fe53d91efa4693b66bda0132e38ea66038ff2009
keith <1033610335@qq.com>
2025-12-05 20:33:31 +0800

    chore: replace fake() helper in DatabaseSeeder for production compatibility

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        fake()->seed(20250920);
+        Factory::create()->seed(20250920);
 
         $profile = config('seeding.profile', 'demo');
 
